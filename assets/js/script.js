8e3df65cfca56973a7c89b375b95d1f5c9b0c0c8
Eduardo Pinheiro <72766650+odraudep@users.noreply.github.com>
2020-11-08 21:36:54 -0300

    clound

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,15 @@ let sky = document.body;
 let data = new Date()
 let hora = data.getHours()
 
-hora = 2
+hora = 22
 
+// start
 if (hora >= 6 && hora <= 7 || hora >= 17 && hora <= 18) {
-  sky.style.backgroundColor = '#f0b52b'
+  sky.style.backgroundColor = '#feae02'
+  //day
 } else if (hora >= 7 && hora <= 16) {
-  sky.style.backgroundColor = '#4aa4f7'
+  sky.style.backgroundColor = '#0189d6'
+  // night
 } else {
-  sky.style.backgroundColor = '#000c5e'
+  sky.style.backgroundColor = '#101f48'
 }
